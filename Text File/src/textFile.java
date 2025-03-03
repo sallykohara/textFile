79acc4e40aed52eb6b742f2c9f05a7b3777fe30d
sally.kohara <sally.kohara@MU-C1-11.mullen.pvt.k12.co.us>
2025-03-03 12:40:18 -0700

    poop

---
+++ Text File/src/textFile.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner; 
+
+public class textFile
+	{
+		
+		public static void main(String[] args) throws IOException
+			{
+				ArrayList<textFile> results = ();
+				getTextFile();
+			}
+			
+		public static void getTextFile() throws IOException
+		{
+			Scanner file = new Scanner(new File("newTextFile.txt"));
+			while(file.hasNext())
+				{
+					
+				}
+		}
+
+	}
